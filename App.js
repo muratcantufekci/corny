ae97cd24751bba7f2a0d5a7fee6df38b6ea5a61c
muratcan.tufekci <muratcan.tufekci@demirorenmedya.com>
2024-07-06 00:50:05 +0300

    sdk updated, metro config for svg and splahs screen done

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import WelcomeScreen from "./src/screens/WelcomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import MailSignUpScreen from "./src/screens/MailSignUpScreen";
 import ProfileSelectionScreen from "./src/screens/ProfileSelectionScreen";
+import { useEffect, useState } from "react";
+import HomeScreen from "./src/screens/HomeScreen";
+import Logo from './assets/logo.svg'
+import OnboardingStartScreen from "./src/screens/onboarding/OnboardingStartScreen";
 // import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Stack = createStackNavigator();
@@ -40,8 +44,8 @@ const AuthStack = () => (
     }}
   >
     <Stack.Screen
-      name="Welcome"
-      component={WelcomeScreen}
+      name="OnboardingStart"
+      component={OnboardingStartScreen}
       options={{ headerShown: false }}
     />
     <Stack.Screen name="MailSignup" component={MailSignUpScreen}/>
@@ -57,18 +61,55 @@ const AuthStack = () => (
 // );
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.splashContainer}>
+          <Logo width={120} height={40} style={styles.splasgImg}/>
+          <StatusBar style="dark" />
+      </View>
+    );
+  }
   const isLoggedIn = false;
   return (
     <View style={styles.wrapper}>
       <NavigationContainer theme={{ colors: { background: "transparent" } }}>
-        {isLoggedIn ? <Text>Hello</Text> : <AuthStack />}
+        {isLoggedIn ? <Text>Hello</Text> :  <AuthStack />}
       </NavigationContainer>
       <StatusBar style="dark" />
     </View>
   );
+
+  // const isLoggedIn = false;
+  // return (
+  //   <View style={styles.wrapper}>
+  //     <NavigationContainer theme={{ colors: { background: "transparent" } }}>
+  //       {isLoggedIn ? <Text>Hello</Text> : <AuthStack />}
+  //     </NavigationContainer>
+  //     <StatusBar style="dark" />
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
+  splashContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FF524F',
+  },
+  splasgImg: {
+    color: '#FFFFFF'
+  },
   wrapper: {
     flex: 1,
   },
